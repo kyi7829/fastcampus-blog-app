2b5a4e4889bc614ceb372f462e60e577ab52f7e0
kyi7829 <yi7829@naver.com>
2024-12-19 21:35:41 +0900

    user 관리 context 적용

--- src/context/AuthContext.tsx
+++ src/context/AuthContext.tsx
@@ -1,5 +1,5 @@
-import { createContext, ReactNode, useState } from "react";
-import { getAuth, User } from "firebase/auth";
+import { createContext, ReactNode, useEffect, useState } from "react";
+import { getAuth, onAuthStateChanged, User } from "firebase/auth";
 import { app } from "firebaseApp";
 
 interface AuthProps {
@@ -13,4 +13,22 @@ const AuthContext = createContext({
 export const AuthContextProvider = ({ children }: AuthProps) => {
     const auth = getAuth(app);
     const [currentUser, setCurrentUser] = useState<User | null>(null);
+
+  useEffect(() => {
+    onAuthStateChanged(auth, (user) => {
+      if (user) {                
+        setCurrentUser(user); 
+      } else {
+        setCurrentUser(user);
+      }
+    });
+  }, [auth]);    
+
+  return (
+     <AuthContext.Provider value={{user: currentUser}}>
+        {children}
+    </AuthContext.Provider>
+  );
 }
+
+export default AuthContext;
