3e738268c2ed2049535371ab1057a78ab5959617
YeongInKim <yi7829l@naver.com>
2024-12-20 13:01:17 +0900

    PostForm 개발중 commit

--- src/components/PostForm.tsx
+++ src/components/PostForm.tsx
@@ -1,17 +1,41 @@
+import React, { useState } from "react";
+
 export default function PostForm() {
+    const [title, setTitle] = useState<string>("");
+    const [summary, setSummary] = useState<string>("");
+    const [content, setContent] = useState<string>("");
+
+    const onChange = (e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
+        const { 
+            target: {name, value}
+        } = e;
+
+        if (name === "title") {
+            setTitle(value);
+        }
+
+        if (name === "summary") {
+            setSummary(value);
+        }
+
+        if (name === "content") {
+            setContent(value);
+        }                
+    }
+
     return (
         <form action="/post" method="POST" className="form">
             <div className="form__block">
                 <label htmlFor="title">제목</label>
-                <input type="text" name="title" id="title" required />
+                <input type="text" name="title" id="title" required onChange={onChange} value={title}/>
             </div>
             <div className="form__block">
                 <label htmlFor="summary">요약</label>
-                <input type="text" name="summary" id="summary" required />
+                <input type="text" name="summary" id="summary" required onChange={onChange} value={summary}/>
             </div>            
             <div className="form__block">
                 <label htmlFor="content">내용</label>
-                <textarea name="content" id="content" required />
+                <textarea name="content" id="content" required onChange={onChange} value={content}/>
             </div>                        
             <div className="form__block">
                 <input type="submit" value="제출" className="form__btn--submit" />
