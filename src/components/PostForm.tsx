f233ff3dd2381eee4ed193df18cfe638921f911d
YeongInKim <yi7829l@naver.com>
2024-12-23 12:48:34 +0900

    게시글 등록 Component fireStore addDoc 적용

--- src/components/PostForm.tsx
+++ src/components/PostForm.tsx
@@ -1,9 +1,38 @@
-import React, { useState } from "react";
+import React, { useContext, useState } from "react";
+import { collection, addDoc } from "firebase/firestore";
+import { db } from "firebaseApp";
+import AuthContext from "context/AuthContext";
+
+import { useNavigate } from "react-router-dom";
+import { toast } from "react-toastify";
 
 export default function PostForm() {
     const [title, setTitle] = useState<string>("");
     const [summary, setSummary] = useState<string>("");
     const [content, setContent] = useState<string>("");
+    const { user } = useContext(AuthContext);
+    const navigate = useNavigate();
+
+    const onSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+
+        try {
+            // firestore로 데이터 생성
+            await addDoc(collection(db, "posts"), {
+                title: title,
+                summary: summary,
+                content: content,
+                createAt: new Date()?.toLocaleDateString(),
+                email: user?.email
+            });
+
+            toast?.success("게시글을 생성했습니다.");
+            navigate("/");            
+        } catch (e: any) {            
+            console.log(e);
+            toast?.error(e?.code);
+        }
+    }
 
     const onChange = (e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
         const { 
@@ -24,7 +53,7 @@ export default function PostForm() {
     }
 
     return (
-        <form action="/post" method="POST" className="form">
+        <form onSubmit={onSubmit} className="form">
             <div className="form__block">
                 <label htmlFor="title">제목</label>
                 <input type="text" name="title" id="title" required onChange={onChange} value={title}/>
